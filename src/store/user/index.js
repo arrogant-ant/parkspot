5bc4192934cad6efce1aaca284d9dfb0d653f00f
Saksham2k3s <sakshamshrivastav58@gmail.com>
2025-05-12 14:20:34 +0530

    pass SpotImages in spot request

--- src/store/user/index.js
+++ src/store/user/index.js
@@ -220,7 +220,7 @@ const actions = {
             Remark: '',
             MapLink: state.contactForm.mapLink,
             ParkingImages: state.contactForm.images,
-            SpotImageURLs: state.contactForm.images
+            SpotImages: state.contactForm.images
         };
 
         console.log("This is final request", req);
