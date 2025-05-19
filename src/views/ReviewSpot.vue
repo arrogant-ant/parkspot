19239d1667f14d3dfbfd14eec53dfcac9f42967b
Saksham2k3s <sakshamshrivastav58@gmail.com>
2025-05-19 12:08:00 +0530

    Added facilities in update spot request

--- src/views/ReviewSpot.vue
+++ src/views/ReviewSpot.vue
@@ -82,6 +82,18 @@
                         />
                     </div>
 
+                     <!-- Facilities -->
+                     <div class="form-field">
+                        <label for="facilities">Facilities:</label>
+                        <MultiSelectInput
+                            :list="facilityOptions"
+                            placeholder="Select one or more facilities"
+                            name="facilities"
+                            v-model="Facilities"
+                            :showBorder="true"
+                        />
+                    </div>
+
                     <!-- City -->
                     <div class="form-field">
                         <label for="city">City:</label>
@@ -411,6 +423,8 @@ import { ParkingSize } from '../constant/enums';
 import { SiteType } from '../constant/enums';
 import { SpotRequestStatus } from '../constant/enums';
 import { RentUnit } from '../constant/enums';
+import MultiSelectInput from '@/components/global/MultiSelectInput.vue';
+import { PARKING_FACILITY } from '@/constant/constant';
 
 export default {
     name: 'ReviewSpot',
@@ -422,17 +436,20 @@ export default {
         ImageGallery,
         ImageUpload,
         LoaderModal,
+        MultiSelectInput,
     },
     data() {
         return {
             clickedButton: null, // Tracks which button is clicked
             isModalOpen: false, // Tracks modal visibility
+            facilityOptions: [...PARKING_FACILITY.SO.FACILITIES_DATA],
             modalContent: {
                 action: '',
                 message: '',
                 title: '',
             },
             initialFormData: {},
+            Facilities: ['Covered', 'Gated']
         };
     },
     computed: {
@@ -462,7 +479,7 @@ export default {
         },
         isFormModified() {
             return (
-                JSON.stringify(this.initialFormData) !==
+               this.isFacilitiesUpdated() || JSON.stringify(this.initialFormData) !==
                 JSON.stringify({
                     SO: this.SO,
                     Rent: this.Rent,
@@ -497,6 +514,7 @@ export default {
             'validateLatLong',
             'validateMobile',
             'validateSpotImageUrl',
+            'setUpdatedFacilities'
         ]),
         setSpotId() {
             this.SO.spotId = this.$route.query.requestId;
@@ -558,6 +576,12 @@ export default {
         },
         confirmSave() {
             const updatedFields = new Set();
+            
+            // Compare Facilities
+            if(this.isFacilitiesUpdated()){
+               updatedFields.add("facilities");
+               this.setUpdatedFacilities(this.Facilities);
+            }
 
             ['SO', 'Rent', 'Booking'].forEach((section) => {
                 for (const key in this[section]) {
@@ -606,8 +630,28 @@ export default {
             this.SO.spotImagesList.splice(index, 1);
             this.spotImagesError.splice(index, 1);
         },
+       
+        // isFacilitiesUpdated is compare initial SO.Facilities with this.Facilities 
+        isFacilitiesUpdated() {
+           if(this.SO.Facilities) {
+             const FacilitiesName = this.SO.Facilities.map((facility) => {
+              return facility.Name;
+           })
+
+           return JSON.stringify(FacilitiesName) === JSON.stringify(this.Facilities);
+           }
+
+           return false;
+        }
     },
     watch: {
+        SO(SODetails) {
+           if(SODetails.Facilities && SODetails.Facilities.length > 0) {
+             this.Facilities = SODetails.Facilities.map((facility) => {
+                return facility.Name;
+             })
+           }
+        },
         status(newStatus) {
             if (newStatus === 'error') {
                 this.alertError(this.statusMessage);
